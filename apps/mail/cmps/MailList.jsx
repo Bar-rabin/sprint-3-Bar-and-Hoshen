a07a62ce99fe698ea0346207d2b9fdf83bb3b050
Bar rabin <barrabin2000@gamil.com>
2025-01-23 17:43:26 +0200

    Add send filter

--- apps/mail/cmps/MailList.jsx
+++ apps/mail/cmps/MailList.jsx
@@ -3,13 +3,13 @@ import { MailPreview } from "./MailPreview.jsx";
 
 
 
-export function MailList({ mails, onRemoveMail }) {
+export function MailList({ mails, onRemoveMail, onToggelStar, starColor }) {
 
     return (
         <ul className='mail-list clean-list'>
             {mails.map(mail =>
                 <li key={mail.id}>
-                    <MailPreview mail={mail} onRemoveMail={onRemoveMail} />
+                    <MailPreview mail={mail} onRemoveMail={onRemoveMail} onToggelStar={onToggelStar} starColor={starColor} />
                 </li>
             )}
 
