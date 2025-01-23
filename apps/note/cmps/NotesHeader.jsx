000f850b3689c03e9bd8e00772dbdb7311d356a2
hoshen katz <hoshen.katz@gmail.com>
2025-01-23 17:45:58 +0200

    Add header search bar

--- apps/note/cmps/NotesHeader.jsx
+++ apps/note/cmps/NotesHeader.jsx
@@ -1,13 +1,20 @@
 export function NoteHeader() {
 
     return (
-        <section className='notes-header '>
+        <section className='notes-header flex spaced-between'>
             <div className='main-logo flex'>
                 <img className="ham" src="icons/hamburger.svg" alt="" />
                 <img className='logo' src='icons/logonotes.png' />
                 <h3>KEEP</h3>
             </div>
-            
+            <article className="search-bar">
+                <form action="search">
+                    <input className="search-input" type="search" placeholder="search" />
+                   
+
+                </form>
+                        <img className="search-icon" src="icons/search.svg" alt="" />
+            </article>
         </section>
     )
 }
