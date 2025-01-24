9edc05a2452123761939cc6af8ee9690376ed04c
hoshen katz <hoshen.katz@gmail.com>
2025-01-24 14:09:32 +0200

    Fine tune styles

--- apps/note/cmps/NoteList.jsx
+++ apps/note/cmps/NoteList.jsx
@@ -22,7 +22,9 @@ export function NoteList({ notes, onRemoveNote }) {
             {notes.map(note =>
             <div className="note-cont" style={note.style} >
                     <section>
-                        <button className="posd-button" onClick={() => onRemoveNote(note.id)}>X</button>
+                        <button className="posd-button" onClick={() => onRemoveNote(note.id)}>
+                            <img className="x"src="icons/x.svg" alt="" />
+                        </button>
                     </section>
 
                 < li key={note.id}  className="clean-list flex  note" onClick={(ev)=> onEditNote(ev, note.id)}>
