a87d9b8d2b2c71c44d40f310f479661f48436011
hoshen katz <hoshen.katz@gmail.com>
2025-01-26 10:30:04 +0200

    Fix mail icons src

--- apps/mail/cmps/MailHeader.jsx
+++ apps/mail/cmps/MailHeader.jsx
@@ -5,21 +5,21 @@ export function MailHeader({ onToggleModal, onSetFilter, inboxCount }) {
     return (
         <section className='mail-header'>
             <div className='main-logo'>
-                <img className='hamburger' src='/icons/hamburger.png' />
-                <img className='logo' src='/icons/asset 1.png' />
+                <img className='hamburger' src='icons/hamburger.png' />
+                <img className='logo' src='icons/asset 1.png' />
             </div>
             <button className='compose-btn' onClick={onToggleModal}>
                 <img className='icon-compose' src='/icons/asset 17.png' /> Compose</button>
             <p className='icon-header' onClick={() => onSetFilter('user@appsus.com')} >
-                <img src='/icons/asset 18.png' />{'Inbox'}</p>
+                <img src='icons/asset 18.png' />{'Inbox'}</p>
             <p className='icon-header' >
-                <img src='/icons/asset 19.png' />Starred</p>
+                <img src='icons/asset 19.png' />Starred</p>
             <p className='icon-header' onClick={() => onSetFilter('momo@momo.com')} >
-                <img src='/icons/asset 21.png' />Sent</p>
+                <img src='icons/asset 21.png' />Sent</p>
             <p className='icon-header' >
-                <img src='/icons/asset 22.png' />Draft</p>
+                <img src='icons/asset 22.png' />Draft</p>
             <p className='icon-header' >
-                <img src='/icons/asset 30.png' />Trash</p>
+                <img src='icons/asset 30.png' />Trash</p>
 
 
         </section>
